973c70cfa9fc4ba8ea48694ebf057d3f7e19bd5b
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-27 20:35:36 +0300

    Adding the image model and its methods

--- pictures/models.py
+++ pictures/models.py
@@ -33,3 +33,38 @@ class categories(models.Model):
     class Meta:
         ordering = ['category']       
        
+       
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name =  models.CharField(max_length =60)
+    description = models.TextField()
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    categories = models.ManyToManyField(categories) 
+    
+    def __str__(self):
+        return self.name
+    
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()    
+    
+    def get_image_by_id(id):
+        image = Image.objects.get(id)
+        return image
+        
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images    
+    
+    @classmethod
+    def search_image(cls,search_term):
+        images = cls.objects.filter(categories__category=search_term)
+        return images
+    
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = cls.objects.filter(location__location=location).all()
+        return image_location       
