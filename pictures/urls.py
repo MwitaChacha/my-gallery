4e2ef8773d68da50247f4774601cc1659db0c0a2
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-27 20:55:56 +0300

    Adding the display view function

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
    path('',views.index, name='index'),
+   path('display/',views.display, name='display'),
+   path('search/',views.search_results, name='search_results'),
+   path('location/<location>/',views.location_results, name='location_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- pictures/views.py
+++ pictures/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
        
     return render(request, 'index.html')
+
+def display(request):
+    images = Image.objects.all()
+    location = Location.objects.all()
+    return render(request, 'display.html',{"images":images, "location":location})
