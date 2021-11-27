7e1391d41f60ddd5ef5dc375139ae00a0ff3b2b8
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-27 20:34:01 +0300

    Adding the location model and its methods

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+class Location(models.Model):
+    location = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return self.location 
+    
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    class Meta:
+        ordering = ['location']
+       
